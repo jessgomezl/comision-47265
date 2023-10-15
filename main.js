789b3fa9504eac56b8090478073f3ed08ad379da
jessica gomez <jessicajohannagomezl@gmail.com>
2023-10-15 03:15:18 -0300

    introduci length en categorias y map, join e index en productos

--- main.js
+++ main.js
@@ -1,40 +1,65 @@
 function saludar(){
     let nombre = prompt("Ingrese su nombre");
-    if (nombre === ""){
+    if (!nombre){
         alert("No ingreso correctamente su nombre. Porfavor intentelo nuevamente");
     }else{
         alert(`${nombre} te damos la bienvenida a tienda VICKYS`);
     }
 }
 
-function finalizar(){
-    alert("¡Muchas gracias por visitar nuestra tienda!");}
+function mostrarCategoriasYProductos() {
+    const categorias = [
+        "Accesorios", 
+        "Belleza", 
+        "Manicuría"
+    ];
+    const productos = {
+        Accesorios: [
+            { nombre: "Billetera Rosas", precio: 1500 },
+            { nombre: "Paraguas Estampados", precio: 2100 },
+            { nombre: "Organizador de Viaje", precio: 1800 },
+        ],
+        Belleza: [
+            { nombre: "Brocha Pincel", precio: 1500 },
+            { nombre: "Espejo", precio: 1200 },
+            { nombre: "Peine", precio: 1100 },
+        ],
+        Manicuría: [
+            { nombre: "Set Manicuría 5 Piezas", precio: 2200 },
+            { nombre: "Set Manicuría 9 Piezas", precio: 2500 },
+            { nombre: "Set Manicuría 10 Piezas", precio: 2600 },
+        ],
+    };
+
+    const seleccionCategoria = Number(prompt("Elige una categoría:\n1) Accesorios\n2) Belleza\n3) Manicuría"));
+
+    if (seleccionCategoria >= 1 && seleccionCategoria <= categorias.length) {
+        const categoriaElegida = categorias[seleccionCategoria - 1];
+        alert(`Has elegido la categoría: ${categoriaElegida}`);
+
+        const productosCategoria = productos[categoriaElegida];
+
+        const seleccionProducto = Number(prompt(`Seleccione el producto a buscar "${categoriaElegida}":\n${productosCategoria.map((producto, index) => `${index + 1}) ${producto.nombre}`).join("\n")}`));
+
+        if (seleccionProducto >= 1 && seleccionProducto <= productosCategoria.length) {
+            const productoElegido = productosCategoria[seleccionProducto - 1];
+                alert(`Has elegido el producto: ${productoElegido.nombre} - Precio: ${productoElegido.precio}`);
+        } else {
+            alert("Selección de producto no encontrado. Por favor, elige una opción correcta.");
+        }
+
+    } else {
+        alert("Selección de categoría no encontrado. Por favor, elige una categoría correcta.");
+    }
+    
+    finalizar();
+}
+
+function finalizar() {
+    alert("¡Muchas gracias por visitar nuestra tienda!");
+}
 
 saludar ();
 
-let registrarse = Number(prompt("Quiere registrarse como: \n1 Mayorista \n2 Minorista"));
+mostrarCategoriasYProductos();
 
-if  (registrarse === 1){
-    alert("Ya podras contar con nuestros beneficios especiales");
-
-    let categoria = Number(prompt("Elige una categoria para ver nuestros productos \n1 ACCESORIOS \n2  BELLEZA \n3  MANICURIA"));
-    while (categoria != ("")){
-        switch (categoria){
-            case 1:
-                alert("1 billetera rosas $1500 \n2 paraguas estampado $2100 \n3 organizador de viaje $1800");
-            break;
-            case 2:
-                alert("1 brocha pincel $1500 \n2 espejo $1200");
-            break;
-            case 3:
-                alert("1 set manicuria 5 piezas $2100 \n2 set manicuria 9 piezas $2500");
-            break;
-            default:
-                alert("ingrese la categoria deseada");
-            break;
-            }
-        categoria = Number(prompt("Elige una categoria para ver nuestros productos \n1 ACCESORIOS \n2  BELLEZA \n3  MANICURIA"));
-    }
-}else if(registrarse === 2){
-    alert("No podras aprovechar de descuentos ni beneficios");
-} finalizar();
