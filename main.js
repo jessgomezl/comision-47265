272f834ae935ab827e22aa98c503a008c41f4004
jessica gomez <jessicajohannagomezl@gmail.com>
2023-11-01 23:11:26 -0300

    modificando para la 3era entrega

--- main.js
+++ main.js
@@ -1,76 +1,74 @@
-function saludar(){
-    let nombre = prompt("Ingrese su nombre");
-    if (!nombre){
-        alert("No ingreso correctamente su nombre. Porfavor intentelo nuevamente");
-    }else{
-        alert(`${nombre} te damos la bienvenida a tienda VICKYS`);
-    }
+const categorias = [
+    "Accesorios", 
+    "Belleza", 
+    "Manicuría"
+];
+const productos = {
+    Accesorios: [
+        { nombre: "Billetera Rosas", precio: 1600 },
+        { nombre: "Paraguas Estampados", precio: 2100 },
+        { nombre: "Organizador de Viaje", precio: 1800 },
+    ],
+    Belleza: [
+        { nombre: "Brocha Pincel", precio: 1500 },
+        { nombre: "Espejo", precio: 1200 },
+        { nombre: "Peine", precio: 1100 },
+    ],
+    Manicuría: [
+        { nombre: "Set Manicuría 5 Piezas", precio: 2200 },
+        { nombre: "Set Manicuría 9 Piezas", precio: 2500 },
+        { nombre: "Set Manicuría 10 Piezas", precio: 2600 },
+    ],
+};
+
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+function agregarAlCarrito(categoria, productoIndex) {
+    const producto = productos [categoria] [productoIndex];
+    carrito.push(producto);
+    actualizarCarrito();
 }
 
-function mostrarCategoriasYProductos(){
-    const categorias = [
-        "Accesorios", 
-        "Belleza", 
-        "Manicuría"
-    ];
-    const productos = {
-        Accesorios: [
-            { nombre: "Billetera Rosas", precio: 1600 },
-            { nombre: "Paraguas Estampados", precio: 2100 },
-            { nombre: "Organizador de Viaje", precio: 1800 },
-        ],
-        Belleza: [
-            { nombre: "Brocha Pincel", precio: 1500 },
-            { nombre: "Espejo", precio: 1200 },
-            { nombre: "Peine", precio: 1100 },
-        ],
-        Manicuría: [
-            { nombre: "Set Manicuría 5 Piezas", precio: 2200 },
-            { nombre: "Set Manicuría 9 Piezas", precio: 2500 },
-            { nombre: "Set Manicuría 10 Piezas", precio: 2600 },
-        ],
-    };
+function actualizarCarrito(){
+    const carritoDiv = document.getElementById("carrito");
+    carritoDiv.innerHTML = "";
 
-    const seleccionCategoria = Number(prompt("Elige una categoría:\n1)Accesorios \n2)Belleza \n3)Manicuría"));
-
-    if (seleccionCategoria >= 1 && seleccionCategoria <= categorias.length){
-        const categoriaElegida = categorias[seleccionCategoria - 1];
-            alert(`Has elegido la categoría: ${categoriaElegida}`);
-
-        const productosCategoria = productos[categoriaElegida];
-            alert(`Productos en la categoría ${categoriaElegida}:`);
-        productosCategoria.forEach((producto, index) => {
-            alert(`
-            ${index + 1}) ${producto.nombre} 
-            Precio: ${producto.precio}
-            `);
-        });
-
-        const precioMinimo = Number(prompt("Filtrar productos por precio minimo. Ingresa el precio"));
-        const productosFiltrados = productosCategoria.filter(producto => producto.precio <= precioMinimo);
-
-        if (productosFiltrados.length > 0) {
-                alert("Productos que cumplen con el precio:");
-            productosFiltrados.forEach(producto => {
-                alert(`
-                    Nombre: ${producto.nombre} 
-                    Precio: ${producto.precio}
-                    `);
-            });
-        } else{
-            alert("No hay productos que cumplan con el filtro de ese precio");
-        }
-    } else{
-        alert("Selección de categoría no encontrado. Por favor, elige una categoría correcta");
-    }
-    finalizar();
+    carrito.forEach((productos, index) => {
+        const itemDiv = document.createElement("div");
+        itemDiv.innerHTML = `
+            <h2>${productos.nombre}</h2>
+            <b>${productos.precio}</b>
+            <button onclick="eliminarDelCarrito(${index})">Eliminar</button>
+        `;
+        carritoDiv.appendChild(itemDiv);
+    });
+    localStorage.setItem("carrito",JSON.stringify(carrito));
 }
 
-function finalizar() {
-    alert("¡Muchas gracias por visitar nuestra tienda!");
+function vaciarCarrito () {
+    carrito = [];
+    actualizarCarrito();
 }
 
-saludar ();
+const productosDiv = document.getElementById("productos");
 
-mostrarCategoriasYProductos();
+categorias.forEach((categoria, categoriaIndex) => {
+    const categoriaDiv = document.createElement("div");
+    categoriaDiv.innerHTML = `
+    <h1>${categoria}</h1>
+    `;
+    productosDiv.appendChild(categoriaDiv);
 
+    productos[categoria].forEach((productos, productoIndex) => {
+    const productosDiv = document.createElement("div");
+    productosDiv.innerHTML = `
+        <h3>${productos.nombre}</h3>
+        <b>${productos.precio}</b>
+        <button onclick="agregarAlCarrito('${categoria}', ${productoIndex})">Agregar al Carrito</button>
+    `;
+    categoriaDiv.appendChild(productosDiv)
+    });
+});
+
+const vaciarCarritoButton = document.getElementById("vaciar-carrito");
+vaciarCarritoButton.addEventListener("click", vaciarCarrito);
