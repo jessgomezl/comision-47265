56d380fadd023a79608ddf702566e53e02a37d0c
jessica gomez <jessicajohannagomezl@gmail.com>
2023-12-07 19:50:18 -0300

    correccion total

--- js/main.js
+++ js/main.js
@@ -1,325 +1,59 @@
-const productos ={
-    "Accesorios": [
-    {
-        "id": 1,
-        "nombre": "abanicos",
-        "imagen": "../assets/images/abanico.jpg",
-        "precio": 2100
-    },
-    {
-        "id": 2,
-        "nombre": "billetera animal print",
-        "imagen": "../assets/images/billetera-1.jpg",
-        "precio": 2900
-    },
-    {
-        "id": 3,
-        "nombre": "billetera negra",
-        "imagen": "../assets/images/billetera-2.jpg",
-        "precio": 2900
-    },
-    {
-        "id": 4,
-        "nombre": "billetera roja",
-        "imagen": "../assets/images/billetera-3.jpg",
-        "precio": 2900
-    },
-    {
-        "id": 5,
-        "nombre": "billetera azul",
-        "imagen": "../assets/images/billetera-4.jpg",
-        "precio": 2900
-    },
-    {
-        "id": 6,
-        "nombre": "billetera chocolate",
-        "imagen": "../assets/images/billetera-5.jpg",
-        "precio": 2900
-    },
-    {
-        "id": 7,
-        "nombre": "billetera cuero",
-        "imagen": "../assets/images/billetera-6.jpg",
-        "precio": 2900
-    },
-    {
-        "id": 8,
-        "nombre": "monedero bordo",
-        "imagen": "../assets/images/monedero-1.jpg",
-        "precio": 2100
-    },
-    {
-        "id": 9,
-        "nombre": "monedero cuerina",
-        "imagen": "../assets/images/monedero-2.jpg",
-        "precio": 2100
-    },
-    {
-        "id": 10,
-        "nombre": "monedero rojo",
-        "imagen": "../assets/images/monedero-3.jpg",
-        "precio": 2100
-    },
-    {
-        "id": 11,
-        "nombre": "organizador",
-        "imagen": "../assets/images/organizador.jpg",
-        "precio": 2500
-    },
-    {
-        "id": 12,
-        "nombre": "organizador de viaje",
-        "imagen": "../assets/images/organizadores-de-viaje.jpg",
-        "precio": 2500
-    },
-    {
-        "id": 13,
-        "nombre": "paraguas lisos",
-        "imagen": "../assets/images/paraguas-lisos.jpg",
-        "precio": 3100
-    },
-    {
-        "id": 14,
-        "nombre": "paraguas estampados",
-        "imagen": "../assets/images/paraguas-estampados.jpg",
-        "precio": 3100
-    },
-    {
-        "id": 15,
-        "nombre": "portacelulares",
-        "imagen": "../assets/images/portacelular.jpg",
-        "precio": 2700
-    }
-    ],
-    "Belleza": [
-    {
-        "id": 16,
-        "nombre": "brocha pincel",
-        "imagen": "../assets/images/brocha-1.jpg",
-        "precio": 2700
-    },
-    {
-        "id": 17,
-        "nombre": "brocha ",
-        "imagen": "../assets/images/brocha-2.jpg",
-        "precio": 2700
-    },
-    {
-        "id": 18,
-        "nombre": "brocha",
-        "imagen": "../assets/images/brocha-3.jpg",
-        "precio": 2700
-    },
-    {
-        "id": 19,
-        "nombre": "cepillo ",
-        "imagen": "../assets/images/cepillo-1.jpg",
-        "precio": 2200
-    },
-    {
-        "id": 20,
-        "nombre": "cepillo cuadrado",
-        "imagen": "../assets/images/cepillo-cuadrado.jpg",
-        "precio": 2500
-    },
-    {
-        "id": 21,
-        "nombre": "cepillo redondo",
-        "imagen": "../assets/images/cepillo-redondo.jpg",
-        "precio": 2500
-    },
-    {
-        "id": 22,
-        "nombre": "espejos de bolsillo",
-        "imagen": "../assets/images/espejo-1.jpg",
-        "precio": 2900
-    },
-    {
-        "id": 23,
-        "nombre": "peine con espejo",
-        "imagen": "../assets/images/peine-espejo.jpg",
-        "precio": 2900
-    },
-    {
-        "id": 24,
-        "nombre": "peines",
-        "imagen": "../assets/images/peines-1.jpg",
-        "precio": 2900
-    },
-    {
-        "id": 25,
-        "nombre": "peine cola de rata",
-        "imagen": "../assets/images/peines-2.jpg",
-        "precio": 2900
-    },
-    {
-        "id": 26,
-        "nombre": "peine ",
-        "imagen": "../assets/images/peines-3.jpg",
-        "precio": 2900
-    },
-    {
-        "id": 27,
-        "nombre": "portacosmeticos flor",
-        "imagen": "../assets/images/portacosmeticos-1.jpg",
-        "precio": 2900
-    },
-    {
-        "id": 28,
-        "nombre": "portacosmeticos abeja",
-        "imagen": "../assets/images/portacosmetico-2.jpg",
-        "precio": 2900
-    },
-    {
-        "id": 29,
-        "nombre": "portacosmeticos acua",
-        "imagen": "../assets/images/portacosmetico-3.jpg",
-        "precio": 2900
-    },
-    {
-        "id": 30,
-        "nombre": "portacosmeticos flores",
-        "imagen": "../assets/images/portacosmetico.4.jpg",
-        "precio": 2900
-    }
-    ],
-    "Manicuría": [
-    {
-        "id": 31,
-        "nombre": "alicate",
-        "imagen": "../assets/images/alicate-1.jpg",
-        "precio": 1990
-    },
-    {
-        "id": 32,
-        "nombre": "alicate",
-        "imagen": "../assets/images/alicate-2.jpg",
-        "precio": 1990
-    },
-    {
-        "id": 33,
-        "nombre": "bloque para uñas",
-        "imagen": "../assets/images/bloque-para-uñas-1.jpg",
-        "precio": 2900
-    },
-    {
-        "id": 34,
-        "nombre": "bloque para uñas",
-        "imagen": "../assets/images/bloque-para-uñas-2.jpg",
-        "precio": 2900
-    },
-    {
-        "id": 35,
-        "nombre": "kit manicuria bebe",
-        "imagen": "../assets/images/kit-manicuria-bebe.jpg",
-        "precio": 2900
-    },
-    {
-        "id": 36,
-        "nombre": "set manicuria de 5 piezas",
-        "imagen": "../assets/images/set-manicuria-5-piezas.jpg",
-        "precio": 2900
-    },
-    {
-        "id": 37,
-        "nombre": "set manicuria de 6 piezas",
-        "imagen": "../assets/images/set-manicuria-6-piezas.jpg",
-        "precio": 2900
-    },
-    {
-        "id": 38,
-        "nombre": "set manicuria de 9 piezas",
-        "imagen": "../assets/images/set-manicuria-9-piezas.jpg",
-        "precio": 2900
-    },
-    {
-        "id": 39,
-        "nombre": "set manicuria de 10 piezas",
-        "imagen": "../assets/images/set-manicuria-10-piezas.jpg",
-        "precio": 2900
-    }
-    ]
+let productos = [];
+let carrito = [];
+
+fetch("../js/data.json")
+.then(response => response.json())
+    .then(data => {
+        productos = data;
+        mostrarProductos(productos); 
+    });
+
+function mostrarProductos(productosFiltrados) {
+    const productosDiv = document.getElementById("productos");
+    productosDiv.innerHTML = "";
+
+    productosFiltrados.forEach(producto => { 
+        const productoDiv = document.createElement("div");
+        productoDiv.innerHTML = `
+            <img class="img-producto" src="${producto.imagen}" alt="${producto.nombre}">
+            <h3>${producto.nombre}</h3>
+            <b>$${producto.precio}</b>
+            <button onclick="agregarAlCarrito('${producto.id}')">Agregar al Carrito</button>`;
+        productosDiv.appendChild(productoDiv);
+    });
 }
-document.addEventListener('DOMContentLoaded', function() {
-    const productosContainer = document.getElementById('productosContainer');
-    const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
-    const data = productos;
+function agregarAlCarrito(id) {
+    const productoEncontrado = carrito.find(item => item.id === id);
+    const producto = productos.find(item => item.id === id); 
 
-    for (const categoria in productos) {
-        const categoriaDiv = document.createElement("div");
-        categoriaDiv.innerHTML = `<h1>${categoria}</h1>`;
-        productosContainer.appendChild(categoriaDiv);
-
-        productos[categoria].forEach((producto, productoIndex) => {
-            const productosDeCategoria = document.createElement("div");
-            productosDeCategoria.innerHTML = `
-                <img class="img-producto" src="${producto.imagen}" alt="${producto.nombre}">
-                <h3>${producto.nombre}</h3>
-                <b>$${producto.precio}</b>
-                <button onclick="agregarAlCarrito('${categoria}', ${productoIndex})">Agregar al Carrito</button>
-            `;
-            categoriaDiv.appendChild(productosDeCategoria);
-        });
-    }
-
-    function eliminarDelCarrito(index) {
-        const productoEliminado = carrito[index];
-        carrito.splice(index, 1);
-        actualizarCarrito();
-        Swal.fire(`Producto eliminado del carrito: ${productoEliminado.nombre}`);
-    }
-
-    function actualizarCarrito() {
-        const carritoDiv = document.getElementById("carrito");
-        carritoDiv.innerHTML = "";
-
-        carrito.forEach((producto, index) => {
-            const itemDiv = document.createElement("div");
-            itemDiv.innerHTML = `${producto.nombre} - ${producto.precio} pesos <button onclick="eliminarDelCarrito(${index})">Eliminar</button>`;
-            carritoDiv.appendChild(itemDiv);
-        });
-
-        localStorage.setItem("carrito", JSON.stringify(carrito));
-    }
-
-    function calcularTotalCarrito() {
-        const total = carrito.reduce((accumulator, producto) => accumulator + producto.precio, 0);
-        Swal.fire({
-            text: `El total de la compra es de $${total}`
-        });
-    }
-
-    const calcularTotalButton = document.getElementById("calcularTotal");
-    calcularTotalButton.addEventListener("click", calcularTotalCarrito);
-
-    function agregarAlCarrito(categoria, productoIndex) {
-        const producto = productos[categoria][productoIndex];
+    if (producto && !productoEncontrado) {
         carrito.push(producto);
-        actualizarCarrito();
+        localStorage.setItem("carrito", JSON.stringify(carrito));
+        mostrarProductosEnCarrito();
         Swal.fire({
-            text: `Producto agregado al carrito: ${producto.nombre}`,
-            icon: "success"
+            title: `¡Agregado al carrito!`,
+            text: `Agregar al carrito \n ${producto.nombre} $${producto.precio}`,
+            icon: 'success',
+            confirmButtonText: 'OK'
         });
     }
+}
 
-    function vaciarCarrito() {
-        Swal.fire({
-            title: '¿Estás seguro?',
-            text: 'Esta acción vaciará el carrito',
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Sí, vaciar carrito'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                carrito.length = 0;
-                actualizarCarrito();
-                Swal.fire('¡No hay productos en el carrito!', '', 'success');
-            }
-        });
-    }
+    const accesoriosBtn = document.getElementById("accesoriosBtn");
+    const bellezaBtn = document.getElementById("bellezaBtn");
+    const manicuriaBtn = document.getElementById("manicuriaBtn");
 
-    const vaciarCarritoButton = document.getElementById("vaciarCarrito");
-    vaciarCarritoButton.addEventListener("click", vaciarCarrito);
-});
+    accesoriosBtn.addEventListener("click", function() {
+    const productosAccesorios = productos.filter(producto => producto.id.startsWith("accesorios"));
+        mostrarProductos(productosAccesorios);
+    });
+
+    bellezaBtn.addEventListener("click", function() {
+        const productosBelleza = productos.filter(producto => producto.id.startsWith("belleza"));
+        mostrarProductos(productosBelleza);
+    });
+
+    manicuriaBtn.addEventListener("click", function() {
+        const productosManicuria = productos.filter(producto => producto.id.startsWith("manicuria"));
+        mostrarProductos(productosManicuria);
+    });
